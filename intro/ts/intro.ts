365853957e18245058a6aeb2920b4e09c8e44edc
Andres Moreno <andresmorenojf@gmail.com>
2024-04-01 10:37:38 -0500

    herencia with Ts

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -12,3 +12,20 @@ class Drink {
 
 const drink3 = new Drink('agua');
 console.log(drink3.info());
+
+// Herencia
+class Beer2 extends Drink {
+	private alcohol: number;
+	
+	constructor(name: string, alcohol: number) {
+		super(name);
+		this.alcohol = alcohol
+	};
+
+	info(): string {
+		return `${super.info()} con ${this.alcohol} of alcohol`
+	}
+}
+
+const beer2 = new Beer2('aguila', 1.5);
+console.log(beer2.info());
